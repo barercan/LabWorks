2b757c3cde1501019b3242500a04fbaef46979dc
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-03-22 15:23:52 +0300

    Lab6

--- IT306Lab6.php
+++ IT306Lab6.php
@@ -8,6 +8,17 @@
     <form class="" action="IT306Lab6.php" method="post">
       <input type="text" name="input" value="Input">
       <input type="submit" name="p1" value="Part1">
+      <input type="submit" name="p2" value="Part2">
+      <input type="submit" name="p3" value="Part3">
+      <input type="submit" name="p5" value="Part5">
+    </form>
+
+    <br>
+
+    <form class="" action="IT306Lab6.php" method="post">
+      <input type="text" name="char" value="Char a or c">
+      <input type="text" name="input" value="Number">
+      <input type="submit" name="p4" value="Part4">
     </form>
   </body>
 </html>
@@ -17,7 +28,28 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 if (isset($_POST['p1'])) {
-
+  $number = $_POST['input'];
+  echo part1($number);
+  # code...
+}
+if (isset($_POST['p2'])) {
+  $string = $_POST['input'];
+  echo part2($string);
+  # code...
+}
+if (isset($_POST['p3'])) {
+  $number = $_POST['input'];
+  echo part3($number);
+  # code...
+}
+if (isset($_POST['p4'])) {
+  $char = $_POST['char'];
+  $number = $_POST['input'];
+  echo part4($char, $number);
+  # code...
+}
+if (isset($_POST['p5'])) {
+  $number = $_POST['input'];
   # code...
 }
 
@@ -40,5 +72,34 @@ function part3 ($number){
   }
   while ($number = (int) $number / 10);
   # code...
+}
+function part4 ($char, $x){
+  switch ($char) {
+    case 'a':
+      $a = $x*$x*M_PI;
+      return $a;
+      break;
+
+    case 'c':
+      $c = 2*$x*M_PI;
+      return $c;
+      break;
+
+    default:
+      $d = "You didn't enter appropriate char.";
+      return $d;
+      break;
+  }
+}
+function part5 ($x){
+  $rev = strrev($x);
+  if ($rev == $x) {
+    $r = "It is palindrome";
+    # code...
+  }
+  else {
+    $r = "It is not palindrome";
+  }
+  return $r;
 }
  ?>
