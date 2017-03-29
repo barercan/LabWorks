e886dbab052d80a222e50dfc59767c81a11fd9b6
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-03-29 11:54:46 +0300

    A

--- deneme.php
+++ deneme.php
@@ -1,4 +1,17 @@
 <?php
 for ($i=10; $i > 0; --$i) {
   echo "$i";
+
+  $email = "serdar@vural";
+  for ($i=0; $i < strlen($email) ; $i++) {
+    if ($email[i] == "@") {
+      echo "Bu bir email adresidir.";
+      break;
+      # code...
+    }
+    else {
+      echo "değildir";
+    }
+    # code...
+  }
 } ?>
