8b3b8e3eb2dbb5c9cc390cafdf841c92111018b1
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-03-29 16:55:53 +0300

    a

--- deneme.php
+++ deneme.php
@@ -4,22 +4,26 @@ ini_set('display_errors', 'On');
 for ($i=10; $i > 0; --$i) {
   echo "$i";
 }
-  $email = "serdar@vural";
-  try {
-    for ($i=0; $i < strlen($email) ; $i++) {
-      if ($email[$i] == "@") {
-        echo "Bu bir email adresidir.";
-        break;
-        # code...
-      }
-      else {
-        echo "değildir";
-      }
+$email = "serdar@vural";
+try {
+  for ($i=0; $i < strlen($email) ; $i++) {
+    if ($email[$i] == "@") {
+      echo "Bu bir email adresidir.";
+      break;
       # code...
     }
-  } catch (Exception $e) {
-    echo "Başarısız";
+    else {
+      echo "değildir";
+    }
+    # code...
   }
+} catch (Exception $e) {
+  echo "Başarısız";
+}
 
-  for ($i=0; $i < 10; $i++) {
- ?>
+for ($i=0; $i < 10; $i++) {
+  $array[i] = rand(1,49);
+
+}
+print $array;
+?>
