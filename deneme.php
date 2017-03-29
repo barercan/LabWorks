6aec1840785a5048e57645cfe074ab549678113e
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-03-29 11:57:56 +0300

    A

--- deneme.php
+++ deneme.php
@@ -3,15 +3,20 @@ for ($i=10; $i > 0; --$i) {
   echo "$i";
 
   $email = "serdar@vural";
-  for ($i=0; $i < strlen($email) ; $i++) {
-    if ($email[i] == "@") {
-      echo "Bu bir email adresidir.";
-      break;
+  try {
+    for ($i=0; $i < strlen($email) ; $i++) {
+      if ($email[i] == "@") {
+        echo "Bu bir email adresidir.";
+        break;
+        # code...
+      }
+      else {
+        echo "değildir";
+      }
       # code...
     }
-    else {
-      echo "değildir";
-    }
-    # code...
+  } catch (Exception $e) {
+    echo "Başarısız";
   }
+
 } ?>
