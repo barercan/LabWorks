cd09a2ccf446b20934d0ed023e68313e00eca6cf
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-04-05 17:20:16 +0300

    Lab8B2

---
+++ IT306Lab81.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lab 8-1</title>
+  </head>
+  <body>
+    <form class="" action="IT306Lab81.php" method="post">
+      <input type="number" name="sayi" value="">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  </body>
+</html>
+<?php
+if (isset($_POST["submit"])) {
+  $N = $_POST["sayi"];
+  require("IT306Lab82.php");
+  $array = generateRNum($N);
+  $sortedarray = selectionSort($array);
+  printArr($sortedarray);
+} ?>
---
+++ IT306Lab82.php
@@ -0,0 +1,29 @@
+<?php
+function generateRNum ($N){
+  for ($i=0; $i < $N ; $i++) {
+    $array[$i] = rand(1,1000);
+    return $array;
+  }
+}
+function selectionSort(array $array) {
+    $length = count($array);
+    for($i = 0; $i < $length; $i ++) {
+        $min = $i;
+        for($j = $i + 1; $j < $length; $j ++) {
+            if ($array[$j] < $array[$min]) {
+                $min = $j;
+            }
+        }
+        $tmp = $array[$min];
+        $array[$min] = $array[$i];
+        $array[$i] = $tmp;
+    }
+    return $array;
+}
+function printArr(array $sortedarray){
+  for ($i=0; $i < $N ; $i++) {
+    echo "$sortedarray[$i]<br>";
+  }
+  return true;
+}
+ ?>
