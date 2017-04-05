17f5337264045564e3b37511d54a6c15f0836a4d
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-04-05 16:52:03 +0300

    A

--- IT306Lab8.php
+++ IT306Lab8.php
@@ -12,11 +12,26 @@
   </body>
 </html>
 <?php
+function bubbleSort(array $array) {
+    $array_size = count($array);
+    for($i = 0; $i < $array_size; $i ++) {
+        for($j = 0; $j < $array_size; $j ++) {
+            if ($array[$i] < $array[$j]) {
+                $tem = $array[$i];
+                $array[$i] = $array[$j];
+                $array[$j] = $tem;
+            }
+        }
+    }
+    return $array;
+}
+
 if (isset($_POST["submit"])) {
   $N = $_POST["sayi"];
   for ($i=0; $i < $N ; $i++) {
     $array[$N] = rand(1,1000);
-    echo "$array[$N]<br>";
   }
+  $sortedarray = bubbleSort($array);
+  printArray("$sortedarray");
 }
  ?>
