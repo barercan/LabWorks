d21de4f9cebe757a62452eaceb3f7383d72241c1
Serdar Vural <serdar.vural@stu.khas.edu.tr>
2017-04-05 16:38:26 +0300

    A

---
+++ IT306Lab8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Lab 8</title>
+  </head>
+  <body>
+    <form class="" action="IT306Lab8.php" method="post">
+      <input type="number" name="sayi" value="">
+      <input type="submit" name="submit" value="Submit">
+    </form>
+  </body>
+</html>
+<?php
+if (isset($_POST["submit"])) {
+  $N = $_POST["sayi"];
+  for ($i=0; $i < $N ; $i++) {
+    $array[$N] = rand(1,1000);
+    echo "$array[$N]";
+  }
+}
+ ?>
